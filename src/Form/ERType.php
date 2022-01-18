b0e52e4f42cb84a24de2f9635ae9370f3a0a4b0d
ChatMalin <justin.blanchet@etu.univ-poitiers.fr>
2022-01-18 08:52:43 +0100

    formulaires avec upload de fichiers/photos

--- src/Form/ERType.php
+++ src/Form/ERType.php
@@ -8,18 +8,47 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ERType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('photo')
+            ->add('photo', FileType::class, [
+                'label' => 'Photo (jpeg ou png)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [ // Type mime des fichiers qu'il sera possible de joindre
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => "La photos n'est pas au bon format", ])
+                ],
+                'data_class' => null,
+                'help' => 'Sélectionnez une photo',
+                'row_attr' => ['placeholder' => 'Sélectionnez un fichier'],])
+            ->add('lien', FileType::class, [
+                'label' => 'Lien (PDF)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [ // Type mime des fichiers qu'il sera possible de joindre
+                            'application/pdf',
+                        ],
+                        'mimeTypesMessage' => 'Seul les fichiers PDF sont acceptés', ])
+                ],
+                'data_class' => null,
+                'help' => 'Sélectionnez un PDF',
+                'row_attr' => ['placeholder' => 'Sélectionnez un fichier'],])
             ->add('texte')
-            ->add('lien')
             ->add('icone')
             ->add('coordonnees', PointType::class, [
                 'mapped' => false
