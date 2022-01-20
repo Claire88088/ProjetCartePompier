44a2fd8c3c633f45326661f7c8aa7007ee0a535a
BOUTON CLAIRE <cbouto01@sfasp2mi.univ-poitiers.fr>
2022-01-20 09:58:40 +0100

    ajout req récup points pour affichage

--- req.sql
+++ req.sql
@@ -9,7 +9,7 @@ insert into users('nom', 'roles', 'password') values ('user', '["ROLE_USER"]', '
 /* DELETE FROM commune; */
 insert into commune('nom', 'code_postal') values ('Buxerolles', '86180');
 insert into commune('nom', 'code_postal') values ('Poitiers', '86000');
-insert into commune('nom', 'code_postal') values ('Chätellerault', '86100');
+insert into commune('nom', 'code_postal') values ('Châtellerault', '86100');
 
 /* peuplement icone */
 insert INTO icone('lien') VALUES ('icons8-bâtiment-24.png');
@@ -27,15 +27,15 @@ insert into type_calque('id', 'nom', 'type') VALUES (4, 'Poteaux Incendie', 'PI'
 insert into type_calque('id', 'nom', 'type') VALUES (5, 'Piscines', 'AUTRE');
 
 /* peuplement type_element */
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (1, 'Immeuble', 'IMMEUBLE');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (1, 'Industrie', 'INDUSTRIE');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (2, 'Travaux', 'TRAVAUX');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (3, 'Accès barrière de sécurité', 'ACCES');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (3, 'Echangeur', 'ECHANGEUR');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (3, 'Point kilométrique', 'PK');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (4, 'Poteau incendie', 'PI');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (1, 'Ecole Maternelle', 'AUTRE');
-insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (3, 'Element Auto', 'AUTRE');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (1, 1, 'Immeuble', 'IMMEUBLE');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (2, 1, 'Industrie', 'INDUSTRIE');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (3, 2, 'Travaux', 'TRAVAUX');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (4, 3, 'Accès barrière de sécurité', 'ACCES');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (5, 3, 'Echangeur', 'ECHANGEUR');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (6, 3, 'Point kilométrique', 'PK');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (7, 4, 'Poteau incendie', 'PI');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (8, 1, 'Ecole Maternelle', 'AUTRE');
+insert INTO type_element('id', 'type_calque_id', 'nom', 'type') VALUES (9, 3, 'Element Auto', 'AUTRE');
 
 
 /* peuplement point */
@@ -64,44 +64,11 @@ values (5, 5, 5, NULL, 'pk 1', NULL, NULL, NULL);
 
 php -S localhost:8000 -t public/
 
-/* on récupère tous les éléments / points du calque ER */
-/* on sélectionne les types d'élément correspondant au calque ER */
-SELECT * FROM type_element WHERE type_calque_id = 1 INTO x;
-
-/* on sélectionne les éléments correspondant aux types sélectionnés précédemment */
-SELECT * FROM element WHERE type_element_id = X.id INTO Y;
-
-/* on sélectionne les icones correspondant aux éléments sélectionnés */
-SELECT * FROM icone WHERE id = Y.icone_id;
-
-/* on sélectionne les points correspondant aux éléments sélectionnés */
-SELECT * FROM point WHERE element_id = y.id;
-
-/* avec inner join */
-SELECT * FROM type_element
-WHERE type_calque_id = 1
-INNER JOIN element
-WHERE element.type_element_id = type_element.id
-/*INNER JOIN icone
-WHERE icone.id = element.icone_id*/
-INNER JOIN point
-WHERE point.element_id = element.id 
-
-
-SELECT * FROM element
-INNER JOIN type_element
-WHERE element.type_element_id = type_element.id
-
-SELECT *
-FROM point
-INNER join element on element.id = point.element_id
-
-/* on veut récupérer les points et les icones des éléments qui appartiennent aux types correpondant à un type de calque */ 
-SELECT * from point 
-inner join element on element.id = point.element_id 
-
-select * from icone 
-inner join element on element.icone_id = icone.id 
-
-select * from element
-inner join type_element on type_element.id = element.type_element_id
+/* on veut récupérer les points et les icones des éléments qui appartiennent aux types correpondants à un type de calque */
+/* pour le calque 'ER' */
+select point.latitude, point.longitude, element.photo, element.texte, element.lien, element.date_deb, element.date_fin, type_element.nom, type_calque.nom, icone.couleur, icone.lien from type_calque
+    inner join type_element on type_calque.id = type_element.type_calque_id
+    inner join element on element.type_element_id = type_element.id
+    inner join icone on element.icone_id = icone.id
+    inner join point on point.element_id = element.id
+    where type_calque.type = 'ER'
