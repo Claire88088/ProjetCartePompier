530404cb060c7916626b2178351bee128c704090
BOUTON CLAIRE <cbouto01@sfasp2mi.univ-poitiers.fr>
2022-01-20 09:05:19 +0100

    ordre peuplement

--- req.sql
+++ req.sql
@@ -1,23 +1,15 @@
 
+/* peuplement users */
+/* delete from users; */
+insert into users('nom', 'roles', 'password') values ('admin', '["ROLE_ADMIN"]', '$2y$13$42J55tM7N4R0jdqRc2/4DOrc9o0nt/7huhPRHBalH8bnO0bJ1T3D6');
+insert into users('nom', 'roles', 'password') values ('user', '["ROLE_USER"]', '$2y$13$eD2Ou25gPvOIh4W203mOwe1IyEkN0ttPM309ZD4FfSxD0dMKwmqFW');
+
+
 /* peuplement commune */
-DELETE FROM commune;
+/* DELETE FROM commune; */
 insert into commune('nom', 'code_postal') values ('Buxerolles', '86180');
 insert into commune('nom', 'code_postal') values ('Poitiers', '86000');
 insert into commune('nom', 'code_postal') values ('Chätellerault', '86100');
-
-/* peuplement element */
-insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
-values (1, 1, 1, NULL, 'Ehpad Le Village', NULL, NULL, NULL);
-insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
-values (2, 2, 1, NULL, 'industrie 1', NULL, NULL, NULL);
-insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
-values (3, 2, 4, NULL, 'industrie 2', NULL, NULL, NULL);
-insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
-values (4, 2, 4, NULL, 'industrie 3', NULL, NULL, NULL);
-insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
-values (5, 5, 5, NULL, 'pk 1', NULL, NULL, NULL);
-
-
 
 /* peuplement icone */
 insert INTO icone('lien') VALUES ('icons8-bâtiment-24.png');
@@ -26,18 +18,8 @@ insert INTO icone('lien') VALUES ('icons8-gaz-24.png');
 insert INTO icone('lien') VALUES ('icons8-usine-24.png');
 insert INTO icone('lien') VALUES ('icons8-échelle-24.png');
 
-/* peuplement point */
-delete from point;
-insert into point('element_id', 'latitude', 'longitude', 'rang') values (1, 46.832612, 0.552458, 1);
-insert into point('element_id', 'latitude', 'longitude', 'rang') values (2, 46.812177, 0.554433, 1);
-insert into point('element_id', 'latitude', 'longitude', 'rang') values (3, 46.828285, 0.538806, 1);
-insert into point('element_id', 'latitude', 'longitude', 'rang') values (4, 46.815657, 0.556586, 1);
-insert into point('element_id', 'latitude', 'longitude', 'rang') values (5, 46.825657, 0.556586, 1);
-
-
-
 /* peuplement type_calque */
-DELETE FROM type_calque;
+/* DELETE FROM type_calque;*/
 insert into type_calque('id', 'nom', 'type') VALUES (1, 'Etablissements Répertoriés', 'ER');
 insert into type_calque('id', 'nom', 'type') VALUES (2, 'Travaux', 'TRAVAUX');
 insert into type_calque('id', 'nom', 'type') VALUES (3, 'Autoroute', 'AUTOROUTE');
@@ -56,12 +38,27 @@ insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (1, 'Ecole Mate
 insert INTO type_element('type_calque_id', 'nom', 'type') VALUES (3, 'Element Auto', 'AUTRE');
 
 
+/* peuplement point */
+/* delete from point;*/
+insert into point('element_id', 'latitude', 'longitude', 'rang') values (1, 46.832612, 0.552458, 1);
+insert into point('element_id', 'latitude', 'longitude', 'rang') values (2, 46.812177, 0.554433, 1);
+insert into point('element_id', 'latitude', 'longitude', 'rang') values (3, 46.828285, 0.538806, 1);
+insert into point('element_id', 'latitude', 'longitude', 'rang') values (4, 46.815657, 0.556586, 1);
+insert into point('element_id', 'latitude', 'longitude', 'rang') values (5, 46.825657, 0.556586, 1);
 
 
-/* peuplement users */
-delete from users;
-insert into users('nom', 'roles', 'password') values ('admin', '["ROLE_ADMIN"]', '$2y$13$42J55tM7N4R0jdqRc2/4DOrc9o0nt/7huhPRHBalH8bnO0bJ1T3D6');
-insert into users('nom', 'roles', 'password') values ('user', '["ROLE_USER"]', '$2y$13$eD2Ou25gPvOIh4W203mOwe1IyEkN0ttPM309ZD4FfSxD0dMKwmqFW');
+/* peuplement element */
+insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
+values (1, 1, 1, NULL, 'Ehpad Le Village', NULL, NULL, NULL);
+insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
+values (2, 2, 1, NULL, 'industrie 1', NULL, NULL, NULL);
+insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
+values (3, 2, 4, NULL, 'industrie 2', NULL, NULL, NULL);
+insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
+values (4, 2, 4, NULL, 'industrie 3', NULL, NULL, NULL);
+insert into element('id', 'type_element_id', 'icone_id', 'photo', 'texte', 'lien', 'date_deb', 'date_fin')
+values (5, 5, 5, NULL, 'pk 1', NULL, NULL, NULL);
+
 
 
 
