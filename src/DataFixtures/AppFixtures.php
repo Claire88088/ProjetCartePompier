d22d6bf2f0983cc378787fcf0f78430232be1974
franck <franck@localhost.localdomain>
2022-01-21 09:42:16 +0100

    app fixtures global

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,64 +12,69 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $typeCalqueER= new TypeCalque();
-        $typeCalqueER->setId(1)->setNom('Etablissements Répertoriés')->setType('ER');
+        $typeCalqueER->setNom('Etablissements Répertoriés')->setType('ER');
         $manager->persist($typeCalqueER);
 
-        $typeCalque2 = new TypeCalque();
-        $typeCalque2->setId(2)->setNom('Travaux')->setType('TRAVAUX');
-        $manager->persist($typeCalque2);
+        $typeCalqueTravaux = new TypeCalque();
+        $typeCalqueTravaux->setNom('Travaux')->setType('TRAVAUX');
+        $manager->persist($typeCalqueTravaux);
 
-        $typeCalque3 = new TypeCalque();
-        $typeCalque3->setId(3)->setNom('Autoroute')->setType('AUTOROUTE');
-        $manager->persist($typeCalque3);
+        $typeCalqueAutoroute = new TypeCalque();
+        $typeCalqueAutoroute->setNom('Autoroute')->setType('AUTOROUTE');
+        $manager->persist($typeCalqueAutoroute);
 
-        $typeCalque4 = new TypeCalque();
-        $typeCalque4->setId(4)->setNom('Poteaux Incendie')->setType('PI');
-        $manager->persist($typeCalque4);
+        $typeCalquePI = new TypeCalque();
+        $typeCalquePI->setNom('Poteaux Incendie')->setType('PI');
+        $manager->persist($typeCalquePI);
 
-        $typeCalque5 = new TypeCalque();
-        $typeCalque5->setId(5)->setNom('Piscines')->setType('AUTRE');
-        $manager->persist($typeCalque5);
+        $typeCalquePiscine = new TypeCalque();
+        $typeCalquePiscine->setNom('Piscines')->setType('AUTRE');
+        $manager->persist($typeCalquePiscine);
 
-        //$manager->flush();
+        $manager->flush();
 
-        $typeElement1 = new TypeElement();
-        $calqueERId = $typeCalqueER->getId();
-        $typeElement1->setId(1)->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueERId))->setNom('Immeuble')->setType('IMMEUBLE');
-        $manager->persist($typeElement1);
-/*
-        $typeElement2 = new TypeElement();
-        $typeElement2->setId(2)->setTypeCalque(1)->setNom('Industrie')->setType('INDUSTRIE');
-        $manager->persist($typeElement2);
+        $calqueER = $manager->getRepository('App:TypeCalque')->find($typeCalqueER->getId());
+        $calqueTravaux = $manager->getRepository('App:TypeCalque')->find($typeCalqueTravaux->getId());
+        $calqueAutoroute = $manager->getRepository('App:TypeCalque')->find($typeCalqueAutoroute->getId());
+        $calquePI = $manager->getRepository('App:TypeCalque')->find($typeCalquePI->getId());
+        //$calquePiscine = $manager->getRepository('App:TypeCalque')->find($typeCalquePiscine->getId());
 
-        $typeElement3 = new TypeElement();
-        $typeElement3->setId(3)->setTypeCalque(2)->setNom('Travaux')->setType('TRAVAUX');
-        $manager->persist($typeElement3);
+        $typeElementImmeuble = new TypeElement();
+        $typeElementImmeuble->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueER))->setNom('Immeuble')->setType('IMMEUBLE');
+        $manager->persist($typeElementImmeuble);
 
-        $typeElement4 = new TypeElement();
-        $typeElement4->setId(4)->setTypeCalque(3)->setNom('Accès barrière de sécurité')->setType('ACCES');
-        $manager->persist($typeElement4);
+        $typeElementIndustrie = new TypeElement();
+        $typeElementIndustrie->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueER))->setNom('Industrie')->setType('INDUSTRIE');
+        $manager->persist($typeElementIndustrie);
 
-        $typeElement5 = new TypeElement();
-        $typeElement5->setId(5)->setTypeCalque(3)->setNom('Echangeur')->setType('ECHANGEUR');
-        $manager->persist($typeElement5);
+        $typeElementTravaux = new TypeElement();
+        $typeElementTravaux->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueTravaux))->setNom('Travaux')->setType('TRAVAUX');
+        $manager->persist($typeElementTravaux);
 
-        $typeElement6 = new TypeElement();
-        $typeElement6->setId(6)->setTypeCalque(3)->setNom('Point kilométrique')->setType('PK');
-        $manager->persist($typeElement6);
+        $typeElementAcces = new TypeElement();
+        $typeElementAcces->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueAutoroute))->setNom('Accès barrière de sécurité')->setType('ACCES');
+        $manager->persist($typeElementAcces);
 
-        $typeElement7 = new TypeElement();
-        $typeElement7->setId(7)->setTypeCalque(4)->setNom('Poteau incendie')->setType('PI');
-        $manager->persist($typeElement7);
+        $typeElementEchangeur = new TypeElement();
+        $typeElementEchangeur->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueAutoroute))->setNom('Echangeur')->setType('ECHANGEUR');
+        $manager->persist($typeElementEchangeur);
 
-        $typeElement8 = new TypeElement();
-        $typeElement8->setId(8)->setTypeCalque(1)->setNom('Ecole Maternelle')->setType('AUTRE');
-        $manager->persist($typeElement8);
+        $typeElementPKm = new TypeElement();
+        $typeElementPKm->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueAutoroute))->setNom('Point kilométrique')->setType('PK');
+        $manager->persist($typeElementPKm);
 
-        $typeElement9 = new TypeElement();
-        $typeElement9->setId(9)->setTypeCalque(3)->setNom('Element Auto')->setType('AUTRE');
-        $manager->persist($typeElement9);
-*/
+        $typeElementPI = new TypeElement();
+        $typeElementPI->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calquePI))->setNom('Poteau incendie')->setType('PI');
+        $manager->persist($typeElementPI);
+
+        $typeElementEcoleMat = new TypeElement();
+        $typeElementEcoleMat->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueER))->setNom('Ecole Maternelle')->setType('AUTRE');
+        $manager->persist($typeElementEcoleMat);
+
+        $typeElementEltAutoroute = new TypeElement();
+        $typeElementEltAutoroute->setTypeCalque($manager->getRepository('App:TypeCalque')->find($calqueAutoroute))->setNom('Element Auto')->setType('AUTRE');
+        $manager->persist($typeElementEltAutoroute);
+
         $manager->flush();
     }
 }
