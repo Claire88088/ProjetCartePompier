a531c19f83d6028a0e21cb97afd9544de8f34f95
Claire <claire_bouton@hotmail.com>
2022-01-28 11:19:27 +0100

    ajout contraintes validation type calque

--- src/Entity/TypeCalque.php
+++ src/Entity/TypeCalque.php
@@ -6,9 +6,12 @@ use App\Repository\TypeCalqueRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=TypeCalqueRepository::class)
+ * @UniqueEntity("nom")
  */
 class TypeCalque
 {
@@ -20,7 +23,10 @@ class TypeCalque
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank(message="Le nom du calque est obligatoire")
+     * @Assert\Type("string")
+     * @Assert\Length(max="50")
      */
     private $nom;
 
