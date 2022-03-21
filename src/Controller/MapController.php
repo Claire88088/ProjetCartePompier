ad935985d96e68dce6a1f66941bf12a796df3ec5
BOUTON CLAIRE <cbouto01@sfasp2mi.univ-poitiers.fr>
2022-03-21 15:44:15 +0100

    debug ajout/modif pdf et photo

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -337,13 +337,11 @@ class MapController extends AbstractController
         $elementForm->handleRequest($request);
 
         if ($elementForm->isSubmitted() && $elementForm->isValid()) {
-            if ($_FILES) { // si on a des fichiers téléchargés (photo ou pdf)
-                if (key($_FILES) == "photo") {
-                    $this->photoTreatment($elementForm, $slugger, $element);
-                }
-                if (key($_FILES) == "lien") {
-                    $this->pdfTreatment($elementForm, $slugger, $element);
-                }
+            if ($_FILES[key($_FILES)]["name"]["photo"]) {
+                $this->photoTreatment($elementForm, $slugger, $element);
+            }
+            if ($_FILES[key($_FILES)]["name"]["lien"]) {
+                $this->pdfTreatment($elementForm, $slugger, $element);
             }
 
             $em->persist($element);
@@ -401,13 +399,12 @@ class MapController extends AbstractController
         $elementForm->handleRequest($request);
 
         if ($elementForm->isSubmitted() && $elementForm->isValid()) {
-            if ($_FILES) { // si on a des fichiers téléchargés (photo ou pdf)
-                if (key($_FILES) == "photo") {
-                    $this->photoTreatment($elementForm, $slugger, $elementClique);
-                }
-                if (key($_FILES) == "lien") {
-                    $this->pdfTreatment($elementForm, $slugger, $elementClique);
-                }
+            // si on a des fichiers téléchargés (photo ou pdf)
+            if ($_FILES[key($_FILES)]["name"]["photo"]) {
+                $this->photoTreatment($elementForm, $slugger, $elementClique);
+            }
+            if ($_FILES[key($_FILES)]["name"]["lien"]) {
+                $this->pdfTreatment($elementForm, $slugger, $elementClique);
             }
 
             $em->persist($elementClique);
